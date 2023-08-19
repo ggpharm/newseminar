39bdef4a321314d3de91658d455e88a93e2c38da
SchuleR <i@iprub.ru>
2023-08-19 15:25:00 +0300

    Создал репозиторий

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Домашка 1
+    Console.WriteLine("Введите число A");
+int A = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите число B"); 
+int B = int.Parse(Console.ReadLine());
+
+    Console.WriteLine(A);
+
+    if (A > B)
+{
+    Console.WriteLine("Число " + A + " больше");
+}
+    else
+{
+    Console.WriteLine("Число " + B + " больше");   
+}
