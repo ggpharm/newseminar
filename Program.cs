e3885b5f87293c2f0d00252a107266e4eb64b407
SchuleR <i@iprub.ru>
2023-08-20 15:46:29 +0300

    Сделал задание 6

--- Program.cs
+++ Program.cs
@@ -69,8 +69,17 @@ switch (num)
 
     case 6:
         Console.WriteLine("Вы выбрали задание 6");
-        Console.WriteLine("");
         // Домашка 6
+        Console.WriteLine("Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).");
+        Console.WriteLine("Введите число");
+
+        int X = int.Parse(Console.ReadLine());
+
+        if (X%2 == 0)
+        Console.WriteLine("Число четное");
+        else
+            Console.WriteLine("Число не четное");
+            Console.WriteLine(" ");
         break;
 
     case 8:
