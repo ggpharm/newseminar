1d3c87063382571f0857e85f811ff4e113cbeb79
SchuleR <i@iprub.ru>
2023-08-20 14:03:51 +0300

    Задание 1 выполнено

--- Program.cs
+++ Program.cs
@@ -5,15 +5,20 @@ int A = int.Parse(Console.ReadLine());
     Console.WriteLine("Введите число B"); 
 int B = int.Parse(Console.ReadLine());
 
-    Console.WriteLine(A);
-
+    Console.WriteLine("Отлично, делаем проверку.");
+    Console.WriteLine(" ");
     if (A > B)
 {
     Console.WriteLine("Число " + A + " больше");
+    Console.WriteLine("Число " + B + " меньше");
+
 }
     else
 {
     Console.WriteLine("Число " + B + " больше");   
+    Console.WriteLine("Число " + A + " меньше");  
 }
  
- 
+
+     Console.WriteLine(" ");
+
